1920c125f86a673981cbe06ea2e3c5287d117841
Aoki <ger.vladislav2001@gmail.com>
2020-03-16 10:27:07 +0200

    Update ping.js
    ty discord.js v12 for changings

--- commands/Mao/ping.js
+++ commands/Mao/ping.js
@@ -2,6 +2,6 @@ module.exports = {
     requirements: 'client',
     execute: ( requirements, mao ) => {
         requirements.define( global )
-        addCmd( 'ping pong', 'Checks ping', msg => msg.channel.send( `Ping: \`${Math.floor( client.ping )}ms\`` ) )
+        addCmd( 'ping pong', 'Checks ping', msg => msg.channel.send( `Ping: \`${Math.floor( client.ws.ping )}ms\`` ) )
     }
 }

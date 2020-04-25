141619b9afd242f90dc13d43b8b5a01d7bb2a62c
Aoki <ger.vladislav2001@gmail.com>
2020-04-26 00:15:47 +0300

    A little bit of aesthetics

--- mao.js
+++ mao.js
@@ -1,12 +1,13 @@
 const cp = require( 'child_process' )
-const log = console.log
 
 function start(){
-    let mao = cp.fork( './index.js' )
-    mao.once( 'exit', () => {
-        log( '\nRestarting Mao...\n' )
-        setTimeout( start, 228 )
-    })
+	let mao = cp.fork( __dirname + '/index.js' )
+	
+	mao.once( 'exit', () => {
+		console.log( '\nRestarting Mao...' )
+		setTimeout( start, 228 )
+	})
 }
 
+console.log( '\nStarting Mao...' )
 start()

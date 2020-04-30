00a5d509f2086fae79de924da88cc957d1402365
Aoki <ger.vladislav2001@gmail.com>
2020-04-30 23:47:12 +0300

    Added RGBtoHEX, HEXtoRGB, translit and urban commands

--- commands/Utils/rth.js
+++ commands/Utils/rth.js
@@ -1,7 +1,20 @@
 module.exports = {
-    requirements: 'requirements',
+    requirements: 'embed',
     execute: ( requirements, mao ) => {
         requirements.define( global )
-        // code here
+        
+        addCmd( 'rgbtohex rth', { full: 'Converts RGB to HEX', short: 'no' }, ( msg, args ) => {
+        	let hex = ''
+        	
+        	for( let i = 0; i < 3; ++i ){
+        		let da = Number( args[i] ).toString( 16 )
+        		hex = hex + ( da < 0x10 ? '0' : '' ) + da
+        	}
+        	
+        	msg.send( embed()
+        		.setDescription( `**0x${hex.toUpperCase()}**` )
+       		 	.setColor( hex )
+        	)
+       	})
     }
 }

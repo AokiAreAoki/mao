725e70c3ce483741025e51947eca4e75f697dce0
Aoki <ger.vladislav2001@gmail.com>
2020-05-29 01:38:22 +0300

    Changed to gelbooru.com from aoki/glbr

--- commands/NSFW/boorus.js
+++ commands/NSFW/boorus.js
@@ -49,8 +49,8 @@ module.exports = {
 			let tags = args.join( ' ' )
 			let message = await msg.channel.send( getRandomLoadingPhrase() )
 		
-			//httpGet( `https://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=${tags}&limit=100&json=1`, pics => {
-			httpGet( 'https://aoki.000webhostapp.com/glbr/search/?token=V4OrT6KatVcyHOLaDIVC6yQTNp3RVFKMa47Obwdvee4dc&q=' + tags, pics => {
+			httpGet( `https://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=${tags}&limit=100&json=1`, pics => {
+			//httpGet( 'https://aoki.000webhostapp.com/glbr/search/?token=V4OrT6KatVcyHOLaDIVC6yQTNp3RVFKMa47Obwdvee4dc&q=' + tags, pics => {
 				try {
 					pics = JSON.parse( pics )
 				} catch( err ){

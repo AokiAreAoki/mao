4dd66f3ec010a6cc2beb8f794ffdffdcf162e01a
Aoki <ger.vladislav2001@gmail.com>
2020-06-02 17:19:08 +0300

    Just a small feature
    - changed sendemoji

--- commands/Fun/send_emoji.js
+++ commands/Fun/send_emoji.js
@@ -5,19 +5,20 @@ module.exports = {
         
         addCmd( 'sendemoji emoji e', 'Sends random emoji that matches the keyword', ( msg, args ) => {
             let emojis = client.emojis.cache
-            
+            let all = args[1] && /-+all/i.test( args[1].toLowerCase() )
+
             if( args[0] ){
                 args[0] = args[0].toLowerCase()
                 emojis = emojis.filter( e => e.name.toLowerCase().search( args[0] ) !== -1 )
             }
             
-            if( emojis.size > 0 )
-                if( args[1] && /-+all/i.test( args[1].toLowerCase() ) )
+            if( emojis.size > 0 ){
+                if( all )
                     msg.send( emojis.array().join( ' ' ) )
                 else
                     msg.send( emojis.random().toString() )
-            else
-                msg.send( 'Emoji not found :(' )
+            } else
+                msg.send( `Emoji${all ? 's' : ''} not found :(` )
         })
     }
 }

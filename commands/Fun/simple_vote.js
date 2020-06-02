e895356d923c7b0bccd6a5e47c4836e805e8376e
Aoki <ger.vladislav2001@gmail.com>
2020-06-02 17:28:40 +0300

    Added simplevote command

---
+++ commands/Fun/simple_vote.js
@@ -0,0 +1,16 @@
+module.exports = {
+    requirements: 'embed',
+    execute: ( requirements, mao ) => {
+        requirements.define( global )
+        
+        addCmd( 'simplevote vote', 'simple vote', async ( msg, args, get_string_args ) => {
+            let m = await msg.send( embed()
+                .setAuthor( msg.author.tag, msg.author.avatarURL(64) )
+                .addField( 'Vote:', get_string_args() )
+            )
+            
+            await m.react( '✅' )
+            await m.react( '❌' )
+        })
+    }
+}

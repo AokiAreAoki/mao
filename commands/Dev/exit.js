0bd485af0fd8edcab904ee1117462f93b081f470
Aoki <ger.vladislav2001@gmail.com>
2020-06-02 18:19:42 +0300

    Changed exit reaction emoji

--- commands/Dev/exit.js
+++ commands/Dev/exit.js
@@ -4,7 +4,7 @@ module.exports = {
         requirements.define( global )
         
         addCmd( 'exit die', { short: 'guess what', full: 'r u srsly?' }, async ( msg, args ) => {
-            await msg.react( '574361160455815172' )
+            await msg.react( '717396565114880020' )
             await client.destroy()
             let code = Number( args[0] )
             process.exit( isNaN( code ) ? 0 : code )

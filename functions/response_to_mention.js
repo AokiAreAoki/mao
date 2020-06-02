ab00c60432b65cb607aed13358a8f17320804156
Aoki <ger.vladislav2001@gmail.com>
2020-06-02 15:23:06 +0300

    Fixed some message handler bugs
    + added rections to insults
    + added unshiftMessageHandler that adds message handler to the beginning of handlers queue instead of the end

--- functions/response_to_mention.js
+++ functions/response_to_mention.js
@@ -14,13 +14,20 @@ module.exports = {
 			return responses[r].replace( /USER/, member.displayName )
 		}
 		
-		let mention = /^(mao|мао)([~_\-\s]((ch|s)an|чан|сан|тян))?~?[!.]?$/i
+		let mention = /^(mao|мао)([~_\-\s]((ch|s)an|чан|сан|тян))?~?[!.]?$/i,
+			insult  = /^(mao|мао)([~_\-\s]((ch|s)an|чан|сан|тян))?~?\s*((stupid|shit)\s+bot|baka|бака|дура)\s*~?[!.]?$/i
 		
 		client.on( 'ready', () => {
 			mention = new RegExp( `^(mao|мао|<@!?${client.user.id}>)([~_\\-\\s]((ch|s)an|чан|сан|тян))?~?[!.]?$`, 'i' )
+			insult  = new RegExp( `^(mao|мао|<@!?${client.user.id}>)([~_\\-\\s]((ch|s)an|чан|сан|тян))?~?\\s*((stupid|shit)\\s+bot|baka|бака|дура)\\s*~?[!.]?$`, 'i' )
 		})
 		
 		addMessageHandler( msg => {
+			if( msg.author.id == client.user.id || msg.author.bot ) return
+			
+			if( insult.test( msg.content ) )
+				msg.reply( 'no u' )
+
 			if( mention.test( msg.content ) )
 				msg.send( getRandomResponse( msg.member ) + ( msg.content.matchFirst( /(~?[!.]?)$/ ) || '' ) )
 		})

6c416cab591d6ee9f24e703b495931e7c3d8e8cd
Aoki <ger.vladislav2001@gmail.com>
2020-06-07 18:17:38 +0300

    Fixed missed thing

--- commands/Dev/con_command.js
+++ commands/Dev/con_command.js
@@ -3,17 +3,17 @@ module.exports = {
 	execute: ( requirements, mao ) => {
 		requirements.define( global )
 		
-		function exec( log_channel, command, delete_delay ){
+		function exec( print, command, delete_delay ){
 			cp.exec( command, ( error, stdout, stderr ) => {
 				if( error ){
-					let m = log_channel.send( 'Error:' + cb( error.toString().replace( /error:\s*/i, '' ) ) )
+					let m = print( 'Error:' + cb( error.toString().replace( /error:\s*/i, '' ) ) )
 					if( typeof delete_delay === 'number' ) m.then( m => m.delete( delete_delay * 1e3 ) )
 				} else {
-					let m1 = log_channel.send( 'stdout:' + ( stdout ? cb( stdout ) : '**nothing**' ) )
+					let m1 = print( 'stdout:' + ( stdout ? cb( stdout ) : '**nothing**' ) )
 					let m2 = null
 					
 					if( stderr )
-						m2 = log_channel.send( 'stderr:' + cb( stderr.replace( /error:\s*/i, '' ) ) )
+						m2 = print( 'stderr:' + cb( stderr.replace( /error:\s*/i, '' ) ) )
 					
 					if( typeof delete_delay === 'number' ){
 						let deleteMessage = m => m.delete( delete_delay * 1e3 )
@@ -25,12 +25,12 @@ module.exports = {
 		}
 		
 		addCmd( 'con', { short: 'executes console command', full: "what didn't u get?" }, ( msg, args, get_string_args ) => {
-			exec( msg.channel, get_string_args() )
+			exec( msg.send, get_string_args() )
 		})
 		
 		addCmd( 'cond', { short: 'executes console command and removes the message', full: "what didn't u get?" }, ( msg, args, get_string_args ) => {
 			msg.delete()
-			exec( msg.channel, get_string_args(), 11 )
+			exec( msg.send, get_string_args(), 11 )
 		})
 	}
 }

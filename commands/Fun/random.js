ce357fb7210ad1b82e33e92b56d869bc8a8d66bf
Aoki <ger.vladislav2001@gmail.com>
2020-06-14 16:15:20 +0300

    Added range options to roll command
    + Removed goofy semicolons

--- commands/Fun/random.js
+++ commands/Fun/random.js
@@ -3,22 +3,35 @@ module.exports = {
     execute: ( requirements, mao ) => {
         requirements.define( global )
 
-        addCmd( 'roll', 'Rolls a dice', msg => {
-            let num = Math.round( Math.random() * 99 ) + 1;
-            let str = `**${msg.member.displayName}** rolled a **${num}**`
-            if( num == 100 ) str += '! My congratulations~!';
-            msg.send( str );
+        addCmd( 'roll', 'Rolls a dice', ( msg, args ) => {
+            let min = Number( args[0] ),
+                max = Number( args[1] )
+            
+            if( isNaN( min ) ){
+                min = 0
+                max = 100
+            } else if( isNaN( max ) ){
+                max = min
+                min = 0
+            }
+
+            let num = min + Math.round( Math.random() * ( max - 1 ) ) + 1,
+                str = `**${msg.member.displayName}** rolled a **${num}**`
+            
+            if( num === max ) str += '! What a lucky!'
+            msg.send( str )
+            msg.send( [min,max].toString() )
         })
 
         addCmd( 'select choose', 'Selects one of the given variants', ( msg, args ) => {
-            var selected = args[ Math.floor( Math.random() * ( args.length - 1 ) ) ];
-            msg.send( 'I choose **' + selected + '**' );
+            var selected = args[ Math.floor( Math.random() * ( args.length - 1 ) ) ]
+            msg.send( 'I choose **' + selected + '**' )
         })
 
         addCmd( 'rpc', "No, this isn't Remote Procedure Call. This is Rock Paper Scissors!", ( msg, args ) => {
             let rpc = [ 'rock', 'paper', 'scissors' ],
                 mao = Math.floor( Math.random() * 3 ),
-                user = -1;
+                user = -1
             
             if( args[0] ){
                 args[0] = args[0].toLowerCase()

4b56c5b3d56ad7f5317228d99eee00636a6d5f49
Aoki <ger.vladislav2001@gmail.com>
2020-06-20 22:42:13 +0300

    Process priority
    - TODO: nice for linux

--- mao.js
+++ mao.js
@@ -3,6 +3,12 @@ const cp = require( 'child_process' )
 function start(){
 	let mao = cp.fork( __dirname + '/index.js' )
 	
+	if( process.platform === 'win32' )
+		cp.exec( `wmic process where "ProcessID=${mao.pid}" CALL setpriority "above normal"` )
+	else
+		console.log( `\nmao.js :: DO THE NICE, BAKA\n` )
+		// cp.exec( `nice...` )
+
 	mao.once( 'exit', () => {
 		console.log( '\nRestarting Mao...\n' )
 		setTimeout( start, 228 )

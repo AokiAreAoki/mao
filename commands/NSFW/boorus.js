8e3298dde382a9610b5a789c8e5abcd3624ccea9
Aoki <ger.vladislav2001@gmail.com>
2020-07-09 01:53:10 +0300

    Updated booru commands
    + A bit of new loading phrases
    + Cooldown
    + Requesting more than 1 pics (1-9)
    + Picture anti-repeater
    + And some code improvements/optimisation

--- commands/NSFW/boorus.js
+++ commands/NSFW/boorus.js
@@ -1,5 +1,5 @@
 module.exports = {
-	requirements: 'httpGet embed',
+	requirements: 'client httpGet embed',
 	execute: ( requirements, mao ) => {
 		requirements.define( global )
 		
@@ -14,27 +14,93 @@ module.exports = {
 			'読み込み中...',
 			'Sending nudes...',
 			'Please stand by...',
+			'Calm down, weeb...',
+			'<insert loading text here>',
+			':police_car:',
 			'owo',
-			'Pervert...',
-			'Calm down, weeby...',
-			'<insert loading text here>...',
-			':police_car:...',
+			'no',
+			'pervert',
+			'bruh',
+			'virgin',
+			'umm',
+			'oh',
+			'wow',
+			'rip pp',
+			'hot',
+			'.-.',
+			'...',
 		]
 		
 		let getRandomLoadingPhrase = () => loadingPhrases[ Math.floor( Math.random() * loadingPhrases.length ) ]
 		let maotag = 'amatsuka_mao'
 
+		let usedPics = {},
+			cooldown = {}
+		
+		function cd( user, x ){
+			let id = user.id || user
+			
+			if( typeof id !== 'string' )
+				return
+			
+			if( typeof x === 'number' )
+				return cooldown[id] = Math.max( cd( user ), Date.now() ) + ( 3.1 - Math.sqrt( 10 - x ) ) * 6e3
+			
+			return cooldown[id] || 0
+		}
+		
+		function getNewPics( tags, pics, amount, msg, callback ){
+			if( !usedPics[msg.guild.id] )
+				usedPics[msg.guild.id] = {}
+			
+			let used = usedPics[msg.guild.id],
+				unused = []
+			
+			pics.forEach( pic => {
+				if( !used[pic.id] || Date.now() - used[pic.id] < 3600e3 )
+					unused.push( pic )
+			})
+			
+			for( let i = 0, r; i < amount; ++i ){
+				r = Math.floor( Math.random() * unused.length )
+				
+				if( unused[r] ){
+					callback( unused[r] )
+					used[unused[r].id] = Date.now()
+					unused.splice( r, 1 )
+					continue
+				}
+				
+				r = Math.floor( Math.random() * pics.length )
+				
+				if( pics[r] ){
+					callback( pics[r] )
+					used[pics[r].id] = Date.now()
+					pics.splice( r, 1 )
+				}
+			}
+		}
+		
 		addCmd( 'gelbooru glbr', {
 			short: 'hot girls',
 			full: `Usage: gelbooru [tags]`
 			+ `\nThis is NSFW command and only available in NSFW channels`
 			+ `\nBut you can add "/safe" (or "rating:safe") tag to use it in non-NSFW channel`
-		}, async ( msg, args, cmd ) => {
-			let sfw = false,
+		}, async ( msg, args, get_string_args ) => {
+			if( !msg.member.antispam || msg.member.antispam < Date.now() )
+				msg.member.antispam = Date.now() + 1337
+			else
+				return msg.react( '❌' )
+			
+			if( cd( msg.member ) > Date.now() )
+				return msg.send( `**Cool down, baka!** \`${Math.floor( cd( msg.member, 1 ) - Date.now() ) / 1e3}\` seconds left` )
+
+			let x = 1,
+				sfw = false,
 				force = false
 			
 			for( let i = 0; i < args.length; ++i )
-				if( ( args[i] = args[i].toLowerCase() ).search( ' ' ) !== -1 )
+				if( ( args[i] = args[i].toLowerCase() ).search( ' ' ) + 1 )
 					args[i] = args[i].replace( /\s/g, '_' )
 			
 			for( let i = 0; i < args.length; ++i ){
@@ -43,8 +109,12 @@ module.exports = {
 					sfw = true
 				} else if( args[i] === 'rating:safe' ){
 					sfw = true
-				} else if( msg.author.isMaster() && /^-+force$/i.test( args[i] ) ){
+				} else if( msg.author.isMaster() && /^--+force$/i.test( args[i] ) ){
 					force = true
+					args.splice( i, 1 )
+					--i
+				} else if( /^--+x\d$/i.test( args[i] ) ){
+					x = Math.max( Number( args[i][args[i].length - 1] ), 1 )
 					args.splice( i, 1 )
 					--i
 				} else if( args[i] === 'amatsuka_mao' )
@@ -57,8 +127,8 @@ module.exports = {
 			let tags = args.join( ' ' )
 			let message = await msg.send( getRandomLoadingPhrase() )
 		
-			httpGet( `https://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=${tags}&limit=100&json=1`, pics => {
 			//httpGet( 'https://aoki.000webhostapp.com/glbr/search/?token=V4OrT6KatVcyHOLaDIVC6yQTNp3RVFKMa47Obwdvee4dc&q=' + tags, pics => {
+			httpGet( `https://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=${tags}&limit=100&json=1`, pics => {
 				try {
 					pics = JSON.parse( pics )
 				} catch( err ){
@@ -69,25 +139,38 @@ module.exports = {
 					return
 				}
 				
-				if( pics.length == 0 )
+				if( !/\S/.test( tags ) )
+					tags = 'no tags'
+				
+				if( pics.length === 0 )
 					return message.edit( embed()
 						.setDescription( `Tag(s) \`${tags}\` not found :c` )
 						.setColor( 0xFF0000 )
-					)
+					).then( m => m.edit( '' ) )
 				
-				let r = Math.floor( Math.random() * pics.length )
-				let timeout = Date.now() + 228
-				while( !pics[r] && timeout > Date.now() ) 
-					r = Math.floor( Math.random() * pics.length );
+				getNewPics( tags, pics, x, msg, pic => {
+					let post = embed()
+						.setDescription( `[${tags}](https://gelbooru.com/index.php?page=post&s=view&id=${pic.id})` )
+						.setImage( pic.file_url )
+						.setFooter( 'Powered by gelbooru.com' )
+					
+					if( x === 1 )
+						message.edit( post )
+							.then( m => m.edit( '' ) )
+					else
+						msg.send( post )
+				})
 				
-				if( !/\S/.test( tags ) ) tags = 'no tags'
-				
-				message.edit( embed()
-					.setDescription( `[${tags}](https://gelbooru.com/index.php?page=post&s=view&id=${pics[r].id})` )
-					.setImage( pics[r].file_url )
-					.setFooter( 'Powered by gelbooru.com' )
-				).then( m => m.edit( '' ) )
-			}, err => message.edit( cb( err ) ) )
+				if( !msg.author.isMaster() ) ++x
+				cd( msg.member, x )
+				delete msg.member.antispam
+
+				if( x !== 1 )
+					message.delete( 1337 )
+			}, err => {
+				message.edit( cb( err ) )
+				console.error( err )
+			})
 		})
 		
 		addCmd( 'yandere yndr', {
@@ -96,10 +179,20 @@ module.exports = {
 			+ `\nThis is NSFW command and only available in NSFW channels`
 			+ `\nBut you can add "/safe" (or "s") tag to use it in non-NSFW channel`
 		}, async ( msg, args, get_string_args ) => {
-			let sfw = false, force = false
+			if( !msg.member.antispam || msg.member.antispam < Date.now() )
+				msg.member.antispam = Date.now() + 1337
+			else
+				return msg.react( '❌' )
+			
+			if( cd( msg.member ) > Date.now() )
+				return msg.send( `**Cool down, baka!** \`${Math.floor( cd( msg.member, 1 ) - Date.now() ) / 1e3}\` seconds left` )
+
+			let x = 1,
+				sfw = false,
+				force = false
 		
 			for( let i = 0; i < args.length; ++i )
-				if( ( args[i] = args[i].toLowerCase() ).search( ' ' ) !== -1 )
+				if( ( args[i] = args[i].toLowerCase() ).search( ' ' ) + 1 )
 					args[i] = args[i].replace( /\s/g, '_' )
 			
 			for( let i = 0; i < args.length; ++i ){
@@ -110,6 +203,10 @@ module.exports = {
 					sfw = true
 				} else if( msg.author.isMaster() && /^-+force$/i.test( args[i] ) ){
 					force = true
+					args.splice( i, 1 )
+					--i
+				} else if( /^--+x\d$/i.test( args[i] ) ){
+					x = Math.max( Number( args[i][args[i].length - 1] ), 1 )
 					args.splice( i, 1 )
 					--i
 				} else if( args[i] === 'amatsuka_mao' )
@@ -132,6 +229,9 @@ module.exports = {
 					)
 					return
 				}
+
+				if( !/\S/.test( tags ) )
+					tags = 'no tags';
 				
 				if( pics.length == 0 )
 					return message.edit( embed()
@@ -139,19 +239,29 @@ module.exports = {
 						.setColor( 0xFF0000 )
 					)
 				
-				let r = Math.floor( Math.random() * pics.length )
-				let timeout = Date.now() + 228
-				while( !pics[r] && timeout > Date.now() ) 
-					r = Math.floor( Math.random() * pics.length );
-				
-				if( !/\S/.test( tags ) ) tags = 'no tags';
+				getNewPics( tags, pics, x, msg, pic => {
+					let post = embed()
+						.setDescription( `[${tags}](https://yande.re/post/show/${pic.id})` )
+						.setImage( pic.sample_url )
+						.setFooter( 'Powered by yande.re' )
 					
-				message.edit( embed()
-					.setDescription( `[${tags}](https://yande.re/post/show/${pics[r].id})` )
-					.setImage( pics[r].sample_url )
-					.setFooter( 'Powered by yande.re' )
-				).then( m => m.edit( '' ) )
-			}, err => message.edit( cb( err ) ) )
+					if( x === 1 )
+						message.edit( post )
+							.then( m => m.edit( '' ) )
+					else
+						msg.send( post )
+				})
+				
+				if( !msg.author.isMaster() ) ++x
+				cd( msg.member, x )
+				delete msg.member.antispam
+
+				if( x !== 1 )
+					message.delete( 1337 )
+			}, err => {
+				message.edit( cb( err ) )
+				console.error( err )
+			})
 		})
 	}
 }

1a8bfcd3f7cf9ec35a1cd40c757c1b685a4b40f5
Aoki <ger.vladislav2001@gmail.com>
2020-08-13 16:24:12 +0300

    Added old 7977h to the total uptime

--- functions/activities.js
+++ functions/activities.js
@@ -14,7 +14,7 @@ module.exports = {
 				},
 				{ // Mao Total Uptime + Old Mao Total Uptime (7977 hours)
 					type: 'PLAYING',
-					game: () => db.totaluptime ? `for 7977h + ${ Math.floor( db.totaluptime / 60 ) }h` : 'bruh',
+					game: () => db.totaluptime ? `for ${ Math.floor( db.totaluptime / 60 ) }h` : 'bruh',
 				},
 				{ // Messages per Minute
 					type: 'PLAYING',

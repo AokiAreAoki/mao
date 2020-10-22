867a83eeeb5b7e7272b589b4a3fad63257237616
Aoki <ger.vladislav2001@gmail.com>
2020-10-22 15:59:24 +0300

    Added default avatars support

--- commands/Utils/avatar.js
+++ commands/Utils/avatar.js
@@ -1,7 +1,19 @@
 module.exports = {
-	requirements: 'embed findMem',
+	requirements: 'embed findMem instanceOf',
 	execute: ( requirements, mao ) => {
 		requirements.define( global )
+		
+		function sendAvatar( messageOrChannel, userOrMember ){
+			const user = userOrMember.user || userOrMember
+			
+			if( !instanceOf( user, 'User' ) ) return
+			if( !instanceOf( messageOrChannel, 'Message' ) && !instanceOf( messageOrChannel, 'TextChannel' ) ) return
+			
+			messageOrChannel.send( embed()
+				.setDescription( `${user.equals( client.user ) ? 'My' : `**${user.username}**'s`} avatar` )
+				.setImage( user.displayAvatarURL({ size: 2048, dynamic: true }) )
+			)
+		}
 		
 		addCmd( 'avatar', {
 			short: 'displays user/server avatar',
@@ -10,50 +22,45 @@ module.exports = {
 				+ "\n`avatar @someuser#1337` - `@someuser#1337`'s avatar"
 				+ "\n`avatar server` - server icon"
 		}, ( msg, args, get_string_args ) => {
+			// Author's avatar
 			if( !args[0] )
-				// My avatar
-				msg.send( embed()
-					.setDescription( `**${msg.member.displayName}**'s avatar` )
-				   	.setImage( msg.author.avatarURL({ size: 2048, dynamic: true }) )
+				return sendAvatar( msg, msg.author )
+			
+			// Server icon
+			if( args[0].toLowerCase() === 'server' )
+				return msg.send( embed()
+					.setDescription( `**Server**'s icon` )
+					.setImage( msg.guild.iconURL({ size: 1024, dynamic: true }) )
 				)
-			else {
-				// Server icon
-				if( args[0].toLowerCase() == 'server' )
-					return msg.send( embed()
-						.setDescription( `**Server**'s icon` )
-						.setImage( msg.guild.iconURL({ size: 1024, dynamic: true }) )
-					)
+			
+			
+			// Mao's avatar
+			if( args[0].toLowerCase() === 'mao' )
+				return sendAvatar( msg, client.user )
+			
+			// Exclusion: everyone/here
+			if( msg.mentions.everyone )
+				return msg.send( "Are You Baka?" )
+			
+			let memes = msg.mentions.members.array()
+			
+			// Search for user by name
+			if( memes.length === 0 && typeof findMem === 'function' ){
+				let meme = findMem( msg.guild, get_string_args() )
 				
-				// Exclusion: everyone
-				if( msg.mentions.everyone )
-					return msg.send( "Are You Baka?" )
+				if( meme )
+					sendAvatar( msg, meme.user )
+				else
+					msg.send( 'User not found :c' )
 				
-				let memes = msg.mentions.members.array()
-				
-				if( memes.length == 0 && typeof findMem == 'function' ){
-					let meme = findMem( msg.guild, get_string_args() )
-					
-					if( meme )
-						msg.send( embed()
-							.setDescription( ( meme.user.equals( client.user ) ? 'My' : `**${meme.displayName}**'s` ) + ' avatar' )
-							.setImage( meme.user.avatarURL({ size: 2048, dynamic: true }) )
-						)
-					else
-						msg.send( 'User not found :c' )
-					
-					return
-				}
-				
-				// Mentioned member avatar
-				let meme = memes[0]
-				if( meme ){
-					msg.send( embed()
-						.setDescription( ( meme.user.equals( client.user ) ? 'My' : `**${meme.displayName}**'s` ) + ' avatar' )
-						.setImage( meme.user.avatarURL({ size: 2048, dynamic: true }) )
-					)
-				} else
-					msg.send( "Woops... Sry, something went wrong :c" )
+				return
 			}
+			
+			// Mentioned member avatar
+			if( memes[0] )
+				sendAvatar( msg, memes[0].user )
+			else
+				msg.send( "Woops... Sry, something went wrong :c" )
 		})
 	}
 }

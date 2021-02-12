beb29360fb53234b5ed57ddd1ab10d7de613846d
AokiAreAoki <ger.vladislav2001@gmail.com>
2021-02-12 18:36:23 +0200

    Rewrote activity manager + added custom activities support

--- functions/activities.js
+++ functions/activities.js
@@ -2,67 +2,129 @@ module.exports = {
 	requirements: 'client timer db',
 	init: ( requirements, mao ) => {
 		requirements.define( global )
+	
+		const activityTypes = {
+			PLAYING: true,
+			LISTENING: true,
+			WATCHING: true,
+			COMPETING: true,
+			STREAMING: true,
+			CUSTOM_STATUS: false, // for users only
+		}
 		
-		let nextgame = 0,
-			gameinterval = 300e3,
-			gameid = 0,
-			gametext = '',
-			games = [
-				{ // <= startup game
-					type: 'PLAYING',
-					game: 'wakes up~',
-				},
-				{ // Mao Total Uptime + Old Mao Total Uptime (7977 hours)
-					type: 'PLAYING',
-					game: () => db.totaluptime ? `for ${ Math.floor( process.uptime() / 3600 ) }/${ Math.floor( db.totaluptime / 60 ) } hours` : 'bruh',
-				},
-				{ // Messages per Minute
-					type: 'PLAYING',
-					game: () => msgrate.length + ' msgs/min',
-				},
-				{ // Meme
-					type: 'WATCHING',
-					game: () => { nextgame = Math.min( nextgame, Date.now() + 228 ); return 'Send nudes' }
-				},
-			]
+		function Activity( type, callbackOrString ){
+			if( !callbackOrString )
+				[type, callbackOrString] = ['PLAYING', type]
+			else
+				type = type.toUpperCase()
 
-		var msgrate = []
+			if( !activityTypes[type] )
+				throw new Error( `Unknown activity type: "${type}"` )
+
+			let activity = { type }
+
+			if( typeof callbackOrString === 'function' )
+				activity.invoke = callbackOrString
+			else if( typeof callbackOrString === 'string' )
+				activity.invoke = () => callbackOrString
+			else
+				throw new Error( `Wrong typeof second argument (expected function or string, got ${typeof callbackOrString}` )
+
+			return activity
+		}
 		
-		client.on( 'message', msg => {
-			if( msg.member && !msg.member.bot )
-				msgrate.push( Date.now() + 60e3 )
-		})
-		
-		setInterval( () => {
-			while( msgrate.length !== 0 && msgrate[0] < Date.now() ) msgrate.shift()
-		}, 1337 )
-		
-		function updateActivity(){
-			if( nextgame < Date.now() ){
-				nextgame = Date.now() + gameinterval
-				gameid = gameid % ( games.length - 1 ) + 1
+		class ActivityManager {
+			static id = -1
+			static next = 0
+			static text = '<insert text here>'
+			static interval = 300e3
+			static activities = []
+			static wakeupActivity = Activity( 'wakes up~' )
+
+			static getCurrentActivity(){
+				if( this.id === -1 )
+					return this.wakeupActivity
+
+				let customActivity
+
+				while( customActivity = db.customActivities?.[0] ){
+					if( !isNaN( customActivity.deadline ) && isFinite( customActivity.deadline ) && customActivity.deadline > Date.now() )
+						return customActivity
+
+					db.customActivities.shift()
+				}
+
+				return this.activities[this.id]
+			}
+
+			static update(){	
+				if( this.next < Date.now() ){
+					this.next = Date.now() + this.interval
+					this.id = ++this.id % this.activities.length
+				}
+
+				let activity = this.getCurrentActivity()
+				if( !activity ) return
+				let text = String( activity.invoke( activity ) )
+				
+				if( this.text !== text ){
+					this.text = text
+					client.user.setActivity( text, { type: activity.type } )
+				}
 			}
 			
-			let game = games[gameid]
-			let text = String( typeof game.game === 'function' ? game.game() : game.game )
-			
-			if( gametext !== text ){
-				gametext = text
-				client.user.setActivity( text, { type: game.type } )
+			static reset(){
+				this.id = -1
+				this.text = ''
+				this.next = Date.now() + 15e3
+				timer.create( 'activities', 3, 0, () => ActivityManager.update() )
+			}
+
+			static pushActivity( type, callback ){
+				this.activities.push( Activity( type, callback ) )
+			}
+
+			static pushCustomActivity( deadline, type, callback ){
+				db.customActivities ??= []
+				let activity = Activity( type, callback )
+				activity.deadline = Number( deadline )
+				db.customActivities.push( activity )
+			}
+
+			static setCurrentCustomActivity( deadline, type, callback ){
+				db.customActivities ??= []
+				let activity = Activity( type, callback )
+				activity.deadline = Number( deadline )
+				db.customActivities[0] = activity
 			}
 		}
-
-		function reset_games(){
-			gameid = 0
-			gametext = ''
-			nextgame = Date.now() + 15e3
-			timer.create( 'activities', 1.337, 0, updateActivity )
-		}
-		mao.reset_games = reset_games
+		mao.AM = ActivityManager
 		
 		client.once( 'ready2', () => {
-			client.on( 'ready', reset_games )
-			reset_games()
+			ActivityManager.reset()
+			client.on( 'ready', ActivityManager.reset )
 		})
+
+		// uptime
+		ActivityManager.pushActivity( 'PLAYING', () => {
+			if( typeof db.totaluptime === 'number' )
+				return `for ${ Math.floor( process.uptime() / 3600 ) }/${ Math.floor( db.totaluptime / 60 ) } hours`
+			return 'bruh'
+		})
+		
+		// msg rate
+		const msgrate = []
+		
+		client.on( 'message', msg => {
+			if( msg.member && !msg.author.bot )
+				msgrate.push( Date.now() + 60e3 )
+		})
+
+		setInterval( () => {
+			while( msgrate.length !== 0 && msgrate[0] < Date.now() )
+				msgrate.shift()
+		}, 1337 )
+		
+		ActivityManager.pushActivity( 'PLAYING', () => msgrate.length + ' msgs/min' )
 	}
 }

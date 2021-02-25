43e0fb1cf64485f059dcdb4eda2f7cf9700bd069
AokiAreAoki <ger.vladislav2001@gmail.com>
2021-02-25 06:37:06 +0200

    fix

--- commands/Genshin Impact/resin.js
+++ commands/Genshin Impact/resin.js
@@ -9,7 +9,7 @@ module.exports = {
 		addCmd( 'resin', {
 			short: 'calcs time required to regenerate resin from N to M value',
 			full: `Usage: \`-resin <from> <to>\`\n\nCalcs time required for resin to regenerate from \`<from>\` to \`<to>\`\n\nExamples:`
-			+ '\n`-resin 60` - calcs time required for resin to regenerate from 0 to 60'
+			+ '\n`-resin 60` - calcs time required to regenerate 60 resin'
 			+ '\n`-resin 15 40` - calcs time required for resin to regenerate from 15 to 40'
 			+ '\n`-resin 123 max` - calcs time required for resin to regenerate from 123 to max (160)'
 			+ '\n    `max` also has an alias: `full`'
@@ -20,8 +20,8 @@ module.exports = {
 			if( to )
 				to = to?.toLowerCase()
 			else if( from ){
-				oneArg = true
 				[from, to] = [0, from]
+				oneArg = true
 			}
 
 			if( isNaN( from = parseInt( from ) ) )
@@ -32,6 +32,9 @@ module.exports = {
 			else if( isNaN( to = parseInt( to ) ) )
 				return msg.send( 'Provide a valid number' )
 			
+			if( from < 0 )
+				return msg.send( "start value can't be lower than zero" )
+				
 			if( from > to )
 				return msg.send( "How is your start value bigger than end value?" )
 

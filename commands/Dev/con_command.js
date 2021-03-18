c591906dd4ed8e321423da756f24c12914e0aacc
AokiAreAoki <ger.vladislav2001@gmail.com>
2021-03-18 19:48:50 +0200

    Added color char filter

--- commands/Dev/con_command.js
+++ commands/Dev/con_command.js
@@ -2,27 +2,37 @@ module.exports = {
 	requirements: 'cp',
 	init: ( requirements, mao ) => {
 		requirements.define( global )
+		const deleteMessage = m => m.delete( delete_delay * 1e3 )
 		
 		function exec( channel, command, delete_delay ){
 			cp.exec( command, { timeout: 15e3 }, ( error, stdout, stderr ) => {
 				if( error ){
-					let m = channel.send( 'Error:' + cb( error.toString().replace( /error:\s*/i, '' ) ) )
-					if( typeof delete_delay === 'number' ) m.then( m => m.delete( delete_delay * 1e3 ) )
+					let m = channel.send( 'Error:' + cb( error.toString()
+						.replace( /error:\s*/i, '' )
+						.replace( /\u001b\[\??[\d+;]*\w/gi, '' )
+					))
+
+					if( typeof delete_delay === 'number' )
+						m.then( m => m.delete( delete_delay * 1e3 ) )
 				} else {
-					let m1 = channel.send( 'stdout:' + ( stdout ? cb( stdout ) : '**nothing**' ) )
-					let m2 = null
-					
-					if( stderr )
-						m2 = channel.send( 'stderr:' + cb( stderr.replace( /error:\s*/i, '' ) ) )
+					stdout = stdout
+						? cb( stdout.replace( /\u001b\[\??[\d+;]*\w/gi, '' ) )
+						: '`nothing`'
+
+					let m1 = channel.send( 'stdout:' + stdout ),
+						m2 = stderr && channel.send( 'stderr:' + cb( stderr
+							.replace( /error:\s*/i, '' )
+							.replace( /\u001b\[\??[\d+;]*\w/gi, '' )
+						))
 					
 					if( typeof delete_delay === 'number' ){
-						let deleteMessage = m => m.delete( delete_delay * 1e3 )
 						m1.then( deleteMessage )
-						if( m2 ) m2.then( deleteMessage )
+						m2?.then( deleteMessage )
 					}
 				}
 			})
 		}
+		mao.exec = exec
 		
 		addCmd( 'con', { short: 'executes console command', full: "what didn't u get?" }, ( msg, args, get_string_args ) => {
 			exec( msg, get_string_args() )

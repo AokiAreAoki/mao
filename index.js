9a56aeaa87feacfbd0c567f99089b39f2d24862d
AokiAreAoki <ger.vladislav2001@gmail.com>
2021-04-04 13:45:11 +0300

    Added HTML entities decoder

--- index.js
+++ index.js
@@ -352,6 +352,20 @@ function instanceOf( object, constructorName ){
 	return typeof object === 'object' && object !== null && object.constructor.name === constructorName
 }
 
+const htmlEntities = {
+	nbsp: ' ',
+	quot: '\"',
+	amp: '&',
+	lt: '<',
+	gt: '>',
+}
+
+function decodeHTMLEntities( string ){
+	return string
+		.replace( /&#(\d+);/g, ( m, d ) => String.fromCharCode(d) )
+		.replace( /&(nbsp|amp|quot|lt|gt);/g, ( m, e ) => htmlEntities[e] )
+}
+
 function httpGet( options, callback, errcallback ){
 	const promise = new Promise( ( resolve, reject ) => {
 		let protocol, url = options.path ?? options

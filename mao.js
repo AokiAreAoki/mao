1e67efd937ca234f314d5c15a9a90f132e7fab29
AokiAreAoki <ger.vladislav2001@gmail.com>
2021-04-07 07:28:32 +0300

    Moved log

--- mao.js
+++ mao.js
@@ -1,6 +1,6 @@
 const cp = require( 'child_process' )
 
-function start(){
+function start( firstTime ){
 	let mao = cp.fork( __dirname + '/index.js', process.argv.slice(2) )
 	
 	if( process.platform === 'win32' )
@@ -9,13 +9,14 @@ function start(){
 		//console.log( `\nmao.js :: DO THE NICE, BAKA\n` )
 		// cp.exec( `nice...` )
 
+	if( !firstTime )
+		console.log( `Restarting Mao...` )
+
 	mao.once( 'exit', code => {
 		console.log( `\nMao exited with code ${code}` )
 		
 		if( code == 228 ) // full exit code (will not restart)
 			return console.log( `Full exit code received. Mao won't be restarted. Exit.` )
-		
-		console.log( `Restarting Mao...` )
 		
 		if( code === 0 )
 			start()
@@ -24,4 +25,4 @@ function start(){
 	})
 }
 
-start()
+start( true )

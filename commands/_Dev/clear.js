3c56f6f22b1e483c212b3e42a5234e69f602722b
AokiAreAoki <ger.vladislav2001@gmail.com>
2021-06-08 23:52:38 +0300

    Renamed the "Dev" folder to "_Dev"

---
+++ commands/_Dev/clear.js
@@ -0,0 +1,24 @@
+module.exports = {
+	requirements: 'client',
+	init: ( requirements, mao ) => {
+		requirements.define( global )
+
+		addCmd( 'clear clean purge', { short: 'removes messages', full: 'Usage: `clear <number messages (max 50)>`' }, async ( msg, args ) => {
+			let cnt = args[0] ? Math.floor( Math.min( Number( args[0] ), 50 ) ) : 1
+			
+			if( isNaN( cnt ) || cnt <= 0 ){
+				msg.send( 'You entered invalid number or number is ≤ 0' )
+				return
+			}
+			
+			if( client.user.bot )
+				msg.channel.bulkDelete( cnt + 1 )
+			else
+				msg.channel.fetchMessages({ limit: cnt + 1 })
+					.then( mm => mm.array().forEach( m => m.delete() ) )
+
+			msg.send( cnt + ' message' + ( cnt > 1 ? 's have' : ' has' ) + ' been deleted' )
+				.then( m => m.delete( 3e3 ) )
+		})
+	}
+}
---
+++ commands/_Dev/con_command.js
@@ -0,0 +1,47 @@
+module.exports = {
+	requirements: 'cp',
+	init: ( requirements, mao ) => {
+		requirements.define( global )
+		
+		function exec( channel, command, delete_delay ){
+			cp.exec( command, { timeout: 15e3 }, ( error, stdout, stderr ) => {
+				const deleteMessage = m => m.delete( delete_delay * 1e3 )
+
+				if( error ){
+					let m = channel.send( 'Error:' + cb( error.toString()
+						.replace( /error:\s*/i, '' )
+						.replace( /\u001b\[\??[\d+;]*\w/gi, '' )
+					))
+
+					if( typeof delete_delay === 'number' )
+						m.then( deleteMessage )
+				} else {
+					stdout = stdout
+						? cb( stdout.replace( /\u001b\[\??[\d+;]*\w/gi, '' ) )
+						: '`nothing`'
+
+					let m1 = channel.send( 'stdout:' + stdout ),
+						m2 = stderr && channel.send( 'stderr:' + cb( stderr
+							.replace( /error:\s*/i, '' )
+							.replace( /\u001b\[\??[\d+;]*\w/gi, '' )
+						))
+					
+					if( typeof delete_delay === 'number' ){
+						m1.then( deleteMessage )
+						if( m2 ) m2.then( deleteMessage )
+					}
+				}
+			})
+		}
+		mao.exec = exec
+		
+		addCmd( 'con', { short: 'executes console command', full: "what didn't u get?" }, ( msg, args, get_string_args ) => {
+			exec( msg, get_string_args() )
+		})
+		
+		addCmd( 'cond', { short: 'executes console command and removes the message', full: "what didn't u get?" }, ( msg, args, get_string_args ) => {
+			msg.delete()
+			exec( msg, get_string_args(), 11 )
+		})
+	}
+}

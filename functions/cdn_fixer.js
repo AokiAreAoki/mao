ffd9dbfac11400dd1cf78dc554c18d7da7b30356
AokiAreAoki <ger.vladislav2001@gmail.com>
2021-08-27 22:45:04 +0300

    Added discord cdn fixer

---
+++ functions/cdn_fixer.js
@@ -0,0 +1,16 @@
+module.exports = {
+	requirements: 'MM',
+	init: ( requirements, mao ) => {
+		requirements.define( global )
+		
+		MM.pushHandler( 'cdn_fixer', false, msg => {
+			const badURLs = msg.content.match( /https?:\/\/media\.discordapp\.net\/\S+/g )
+
+			if( badURLs && badURLs.length > 0 )
+				msg.send( badURLs
+					.map( url => url.replace( /^https?:\/\/media\.discordapp\.net/, 'https://cdn.discordapp.com' ) )
+					.join( '\n' )
+				)
+		})
+	}
+}

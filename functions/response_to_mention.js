9b45ac1d487d779de6291d574d4fa5a544a9e2c7
AokiAreAoki <ger.vladislav2001@gmail.com>
2021-08-27 21:47:47 +0300

    Update response_to_mention.js

--- functions/response_to_mention.js
+++ functions/response_to_mention.js
@@ -5,13 +5,16 @@ module.exports = {
 		
 		const responses = [
 			`%user`,
-			`(Hi|Yes), (%user|cutie)`,
+			`(Hi|Yes), (%user|cutie)~`,
 			`no u`,
+			`nya(~)?`,
+			`?`,
 		]
 		
 		function getRandomResponse( member ){
 			let r = Math.floor( Math.random() * responses.length )
 			return responses[r]
+				.replace( /\((.+?)\)\?/g, ( matched, s ) => Math.random() < 0.5 ? s : '' )
 				.replace( /\((.+?)\|(.+?)\)/g, ( matched, a, b ) => Math.random() < 0.5 ? a : b )
 				.replace( /%user/gi, member.displayName.replace( /@/g, '#' ) )
 		}
@@ -20,7 +23,7 @@ module.exports = {
 		// = /^(mao|мао)([~_\-\s]((ch|s)an|чан|сан|тян))?~?\s*((stupid|shit)\s+bot|baka|бака|дура|дурында)\s*~?[!.]?$/i
 		
 		client.on( 'ready', () => {
-			mentionRE = new RegExp( `^(mao|мао|<@!?${client.user.id}>)`, 'i' )
+			mentionRE = new RegExp( `^(?:mao|мао|<@!?(${client.user.id})>)`, 'i' )
 		})
 		
 		MM.pushHandler( 'mention_response', false, msg => {
@@ -30,6 +33,9 @@ module.exports = {
 			const mention = msg.content.matchFirst( mentionRE )
 
 			if( mention ){
+				if( mention === client.user.id )
+					return msg.send( msg.author.toString(), 0 )
+
 				const text = msg.content.substr( mention.length )
 
 				if( /^('?s|\s*is)?\s*((stupid|shit|dumb)\s*bot|baka|бака|дура|дурында|тупая|глупая)\s*~?[!.]?/i.test( text ) )

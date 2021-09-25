97b4c4e3b2bc4132fcd24c086c7bd974388c86b3
AokiAreAoki <ger.vladislav2001@gmail.com>
2021-09-25 16:38:27 +0300

    Updated  command

--- commands/Utils/lmgtfy.js
+++ commands/Utils/lmgtfy.js
@@ -5,35 +5,34 @@ module.exports = {
 
 		addCmd({
 			aliases: 'lmgtfy lmg whatis whats',
+			flags: [
+				['iie', `includes internet explorer`],
+			],
 			description: {
-				short: 'lemme googles that for ya (or someone else)',
+				short: 'let me google that for you (or someone else)',
 				full: 'Googles stuff for you (or someone else)',
 				usages: [
 					[`<search request>`, 'googles $1'],
-					[`<search request>`, `--iie`, 'googles $1 and includes internet expainer'],
 				],
 			},
-			callback: ( msg, args ) => {
+			callback: async ( msg, args ) => {
 				let url = 'https://lmgtfy.com/?q=',
 					q = args.get_string(),
-					iie = '',
+					iie = args.flags.iie ? '&iie=1' : '',
 					whatis = args[-1].toLowerCase().startsWith( 'what' ) ? 'What is ' : '',
 					timeout = 1337 + Math.random() * 3e3
-				
+
 				if( !q )
 					return msg.send( 'Usage: `-help lmgtfy`' )
-
-				if( q.matchFirst( /(^|[\s\n]+)-+iie\b/i ) ){
-					q = q.replace( /(^|[\s\n]+)-+iie\b/i, '' )
-					iie = '&iie=1'
-				}
 
 				url += encodeURI( ( whatis + q ).replace( /\s+/g, '+' ) ) + iie
 
 				msg.send( Embed()
 					.addField( `OK 👌. Googling \`${whatis + q}\`...`, 'Please wait a bit :^)' )
 				).then( m => {
-					setTimeout( () => m.edit( Embed().addField( 'Found!', `Click here to find out ${whatis.toLowerCase()}[${q}](${url})` ) ), timeout )
+					setTimeout( () => {
+						m.edit( Embed().addField( 'Found!', `Click here to find out ${whatis.toLowerCase()}[${q}](${url})` ) )
+					}, timeout )
 				})
 			},
 		})

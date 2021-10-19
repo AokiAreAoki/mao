7afe254d2ad19529ef39785e429b46212ab1e1e7
AokiAreAoki <ger.vladislav2001@gmail.com>
2021-10-19 14:45:09 +0300

    Changed minutes to days

--- commands/Mao/uptimes.js
+++ commands/Mao/uptimes.js
@@ -30,7 +30,7 @@ module.exports = {
 			description: 'Mao\'s total uptime',
 			callback: msg => {
 				let totaluptime = db.totaluptime || 1
-				let string_time = `${numsplit( Math.floor( totaluptime ) )}m = ${numsplit( Math.floor( totaluptime / 6 ) / 10 )}h`
+				let string_time = `${numsplit( Math.floor( totaluptime / 60 / 24 ) )}d = ${numsplit( Math.floor( totaluptime / 6 ) / 10 )}h`
 				msg.send( Embed().addField( 'Total uptime', string_time ) )
 			},
 		})

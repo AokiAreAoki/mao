5ecff8cbaa1f3b53f18535d88ac8b3eff91ca8b4
AokiAreAoki <ger.vladislav2001@gmail.com>
2022-04-12 02:58:59 +0300

    Added banner alias for banners

--- commands/Utils/avatar.js
+++ commands/Utils/avatar.js
@@ -1,73 +1,78 @@
 module.exports = {
-	requirements: 'Embed instanceOf',
+	requirements: 'discord Embed client',
 	init: ( requirements, mao ) => {
 		requirements.define( global )
 		
-		function sendAvatar( messageOrChannel, userOrMember ){
-			const user = userOrMember.user || userOrMember
+		async function sendPFP( messageOrChannel, target, banner ){
+			target = target.user ?? target // user/member/guild
 			
-			if( !instanceOf( user, 'User' ) && !instanceOf( user, 'ClientUser' ) ) return
-			if( !instanceOf( messageOrChannel, 'Message' ) && !instanceOf( messageOrChannel, 'TextChannel' ) ) return
+			const embed = Embed()
+			const isGuild = target instanceof discord.Guild
+			const whose = isGuild ? 'Server' : target.toString() + `'s`
+			const url = banner
+				? ( await target.fetch() ).bannerURL({ size: 2048, dynamic: true })
+				: isGuild
+					? target.iconURL({ size: 1024, dynamic: true })
+					: target.displayAvatarURL({ size: 2048, dynamic: true })
 			
-			messageOrChannel.send( Embed()
-				.setDescription( `${user.equals( client.user ) ? 'My' : `**${user.username}**'s`} avatar` )
-				.setImage( user.displayAvatarURL({ size: 2048, dynamic: true }) )
-			)
+			if( url ){
+				const type = banner ? 'banner' : isGuild ? 'icon' : 'avatar'
+				embed.setDescription( `${whose} ${type}` )
+				embed.setImage( url )
+			} else if( target.accentColor ){
+				const hex = target.accentColor.toString(16).toUpperCase().padStart( 6, '0' )
+				embed.setDescription( `${whose} accent color is \`#${hex}\`` )
+				embed.setColor( target.accentColor )
+			} else
+				embed.setDescription( isGuild
+					? `Server does not have banner`
+					: `${target.toString()} does not have banner nor accent color`
+				)
+			
+			return messageOrChannel.send( embed )
 		}
 		
 		addCmd({
-			aliases: 'avatar',
+			aliases: 'avatar pfp banner',
 			description: {
-				single: `gets user's/server avatar`,
+				short: `gets avatar/banner of an user or server`,
+				full: [
+					`gets avatar/banner of an user or server`,
+					'* use `banner` alias of this command to get banner',
+				],
 				usages: [
-					['gets your avatar'],
-					['server', 'gets server icon'],
-					[`<@@>`, `gets $1's avatar`],
-					[`<username>`, `searches for user and gets their avatar`],
+					['gets your avatar/banner'],
+					['server', 'gets server icon/banner'],
+					[`<@@>`, `gets $1's avatar/banmer`],
+					[`<username>`, `searches for a user and gets their avatar/banner`],
 				],
 			},
 			callback: async ( msg, args ) => {
+				const banner = args[-1] === 'banner'
+				
 				// Author's avatar
 				if( !args[0] )
-					return sendAvatar( msg, msg.author )
+					return sendPFP( msg, msg.author, banner )
 				
 				// Server icon
 				if( args[0].toLowerCase() === 'server' )
-					return msg.send( Embed()
-						.setDescription( `Server icon` )
-						.setImage( msg.guild.iconURL({ size: 1024, dynamic: true }) )
-					)
-				
+					return sendPFP( msg, msg.guild, banner )
 				
 				// Mao's avatar
 				if( args[0].toLowerCase() === 'mao' )
-					return sendAvatar( msg, client.user )
+					return sendPFP( msg, client.user, banner )
 				
 				// Exclusion: everyone/here
 				if( msg.mentions.everyone )
 					return msg.send( "Are You Baka?" )
 				
-				const mentionedMembers = msg.mentions.members
-				
 				// Search for user by name
-				if( mentionedMembers.size === 0 ){
-					const member = await msg.guild.members.find( args.get_string() )
-					
-					if( member )
-						sendAvatar( msg, member.user )
-					else
-						msg.send( 'User not found :(' )
-					
-					return
-				}
+				const member = await msg.guild.members.find( args.get_string() )
 				
-				// Mentioned member avatar
-				const member = mentionedMembers.first()
-
 				if( member )
-					sendAvatar( msg, member.user )
-				else
-					msg.send( "Woops... Sry, something went wrong :(" )
+					return sendPFP( msg, member.user, banner )
+				
+				return msg.send( 'User not found :(' )
 			},
 		})
 	}

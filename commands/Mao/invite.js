51e0346aeac2d1dbdab2e0340629332e029098a2
Aoki <ger.vladislav2001@gmail.com>
2022-06-07 23:37:35 +0300

    Simplified invite command

--- commands/Mao/invite.js
+++ commands/Mao/invite.js
@@ -2,42 +2,15 @@ module.exports = {
 	requirements: 'Embed',
 	init: ( requirements, mao ) => {
 		requirements.define( global )
-		
-		const invite_types = {
-			max: { name: 'Full', int: 8 },
-			min: { name: 'Minimal', int: 3465296 },
-		}
+
+		const PERMS_INT = 3465304
 
 		addCmd({
 			aliases: 'invite',
-			description: {
-				single: 'sends the invite link',
-				usages: [
-					['sends list of all invite types'],
-					['<invite type>', 'sends invite of $1 type'],
-				],
-			},
-			callback: ( msg, args ) => {
-				if( !args[0] ){
-					let types = ''
-					
-					for( let k in invite_types )
-						types += `\n• \`${k}\` - ${invite_types[k].name} permissions invite (integer: ${invite_types[k].int})`
-					
-					if( !types )
-						return msg.send( `Weird... I have no data about invite types` )
-					
-					msg.send( 'Invite types:' + types )
-				}
-				
-				const perm = invite_types[args[0].toLowerCase()]
-					
-				if( perm )
-					msg.send( Embed().addField(
-						perm.name + ' permissions invite',
-						`Here's your [invite link](https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=${perm.int})`
-					))
-			},
+			description: 'sends an invite link',
+			callback: msg => msg.send( Embed().setDescription(
+				`Here's your [invite link](https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=${PERMS_INT})`
+			)),
 		})
 	}
 }

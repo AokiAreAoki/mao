3aae633ce2f6aa87260465671f82450d5fd3421f
AokiAreAoki <ger.vladislav2001@gmail.com>
2022-06-17 20:08:18 +0300

    Better reddit files provider

--- functions/link_utils.js
+++ functions/link_utils.js
@@ -1,7 +1,16 @@
 module.exports = {
-	requirements: 'client MM cp processing',
+	requirements: 'client MM cp processing discord join',
 	init: ( requirements, mao ) => {
 		requirements.define( global )
+
+		function spawnAsync( program, args, options ){
+			return new Promise( resolve => {
+				const ytdl = cp.spawn( program, args, options )
+				let stdout = ''
+				ytdl.stdout.on( 'data', chunk => stdout += chunk )
+				ytdl.once( 'exit', code => resolve( code === 0 ? stdout : null ) )
+			})
+		}
 
 		const utils = [
 			// Discord CDN link fixer
@@ -12,29 +21,44 @@ module.exports = {
 					return badURLs.map( url => url.replace( /^https?:\/\/media\.discordapp\.net/, 'https://cdn.discordapp.com' ) )
 			},
 
-			// Twitter/Reddit direct links provider
+			// Twitter direct links provider
 			async ( msg, react ) => {
-				let links = [].concat( ...[
+				let links = [
 					/https?:\/\/twitter\.com\/[^\/\s]+\/status\/\d+/gmi,
-					/https?:\/\/(?:\w+\.)?reddit.com\/r\/\S+/gmi,
-				].map( re => Array.from( msg.content.matchAll( re ) ) ) )
+				]
+					.map( re => Array.from( msg.content.matchAll( re ) ) )
+					.flat(1)
 
 				if( links.length === 0 )
 					return
 
 				react()
-
-				links = await Promise.all(
-					links.map( url => new Promise( resolve => {
-						const ytdl = cp.spawn( 'youtube-dl', ['--get-url', url[0]] )
-						let stdout = ''
-						ytdl.stdout.on( 'data', chunk => stdout += chunk )
-						ytdl.once( 'exit', code => resolve( code === 0 ? stdout : null ) )
-					}))
-				)
+				links = await Promise.all( links.map( url => spawnAsync( 'youtube-dl', '--get-url', url[0] ) ) )
 
 				return links
 					.map( url => url?.matchFirst( /https?\S+/ ) )
+			},
+
+			// Reddit direct links provider
+			async ( msg, react ) => {
+				let links = [
+					/https?:\/\/(?:\w+\.)?reddit.com\/r\/\S+/gmi,
+				]
+					.map( re => Array.from( msg.content.matchAll( re ) ) )
+					.flat(1)
+
+				if( links.length === 0 )
+					return
+
+				react()
+				const files = await Promise.all( links.map( url => spawnAsync( 'youtube-dl', ['--get-filename', url[0]], { cwd: "/tmp" } ) ) )
+
+				return files
+					.filter( s => !!s )
+					.map( paths => paths.split( '\n' ) )
+					.flat(1)
+					.filter( s => !!s )
+					.map( path => new discord.MessageAttachment( join( '/tmp', path ), path ) )
 			},
 		]
 
@@ -48,25 +72,33 @@ module.exports = {
 				reaction ??= msg.react( processing( '👌' ) )
 			}
 
-			const links = await Promise.all( utils.map( util => util( msg, react ) ) )
-				.then( links => [].concat( ...links )
-					.filter( link => !!link )
+			const files = []
+			const content = await Promise.all( utils.map( util => util( msg, react ) ) )
+				.then( links => links
+					.flat(1)
+					.filter( link => {
+						if( link instanceof discord.MessageAttachment ){
+							files.push( link )
+							return false
+						}
+
+						return !!link
+					})
 					.join( '\n' )
 				)
+				.then( s => s || null )
 
-			let abortHQ = false
+			const hasSomething = content || files.length !== 0
 
-			if( links ){
-				await msg.send( links )
-				abortHQ = true
-			}
+			if( hasSomething )
+				await msg.send({ content, files })
 
 			if( reaction ){
 				await reaction
 				msg.reactions.removeAll()
 			}
 
-			return abortHQ
+			return hasSomething
 		})
 	}
 }

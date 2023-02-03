03113f8b6685b51ec9310cb69d0056ab71cf01fe
AokiAreAoki <ger.vladislav2001@gmail.com>
2023-02-03 18:26:53 +0200

    Add Promise coder

--- re/bakadb.js
+++ re/bakadb.js
@@ -24,6 +24,15 @@ class BakaDB extends events {
 			encode: ( obj, path ) => this._encode( obj, path ),
 			decode: ( str, path ) => this._decode( str, path ),
 		},
+		Promise: {
+			encode: promise => {
+				if( !promise.bakadbIsAwaiting ){
+					promise.bakadbIsAwaiting = true
+					promise.then( () => this.save() )
+				}
+			},
+			decode: () => {},
+		},
 	}
 
 	constructor(){

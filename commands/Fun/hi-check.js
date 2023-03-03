8a4c43255c6f34546f13ab13089d9aa36e050006
Aoki <ger.vladislav2001@gmail.com>
2023-03-03 02:53:30 +0200

    Add funny

--- commands/Fun/hi-check.js
+++ commands/Fun/hi-check.js
@@ -2,6 +2,10 @@
 require = global.alias
 module.exports = {
 	init({ addCommand }){
+		const { Collection } = require( 'discord.js' )
+		const MM = require( '@/instances/message-manager' )
+		const timer = require( '@/re/timer' )
+
 		const states = [
 			"normal",
 			"lgbt",
@@ -12,6 +16,60 @@ module.exports = {
 			"a catgirl (nyahhhh)",
 			"having a ketamine overdose",
 		]
+
+		const hasHi = string => /(\bhi+|:3+)\b/i.test( string )
+		const hiLvl = string => {
+			const match = string.match( /(?:\bh(i+)|:(3+))\b/i )
+			return match
+				? ( match[1] || match[2] ).length - 1
+				: 0
+		}
+
+		let streaks = new Collection()
+
+		timer.create( 'hi', 300, 0, () => {
+			streaks = streaks.filter( streak => {
+				streak.entries = streak.entries.filter( e => e.deadline > Date.now() )
+				return streak.deadline > Date.now()
+			})
+		})
+
+		MM.pushHandler( 'hi', false, msg => {
+			if( !hasHi( msg.content ) )
+				return
+
+			const streak = streaks.get( msg.author.id ) ?? {
+				entries: [],
+				cooldown: 0,
+				deadline: 0,
+			}
+
+			if( streak.cooldown > Date.now() )
+				return
+
+			const lvl = hiLvl( msg.content )
+			const deadline = Date.now() + 60e3
+
+			streak.deadline = deadline
+			streak.entries = streak.entries.filter( e => e.deadline > Date.now() )
+			streak.entries.push({ lvl, deadline })
+			streaks.set( msg.author.id, streak )
+
+			const sumLvl = Math.floor(
+				streak.entries.reduce( ( sum, entry ) => sum + entry.lvl, 0 ) / streak.entries.length
+			)
+
+			console.log( '- Streak:', streak, sumLvl )
+
+			if( sumLvl + streak.entries.length * 2 > 10 ){
+				msg.send([
+					msg.author.toString(),
+					'is having a ketamine overdose',
+				].join( ' ' ) )
+
+				streak.deadline = streak.cooldown = Date.now() + 120e3
+			}
+		})
 
 		addCommand({
 			aliases: 'hi-check check-hi hi',
@@ -25,16 +83,15 @@ module.exports = {
 				await msg.getReferencedMessage()
 					.then( ref => ref && msgs.unshift( ref ) )
 
-				const hiMsg = msgs.find( m => m.id !== msg.id
-					&& !m.isCommand
-					&& /\bhi+\b/i.test( m.content )
+				const hiMsg = msgs.find(
+					m => m.id !== msg.id && !m.isCommand && hasHi( m.content )
 				)
 
 				return hiMsg
 					? msg.send([
 						hiMsg.author.toString(),
 						'is',
-						states.at( hiMsg.content.matchFirst( /\bh(i+)\b/i ).length - 1 ) || states.at(-1),
+						states.at( hiLvl( hiMsg ) ) || states.at(-1),
 					].join( ' ' ) )
 					: msg.send( "No `hi`s found" )
 			}

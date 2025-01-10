758bb6a3989e16ed31bd400fb449c09fefb80f81
Aoki <ger.vladislav2001@gmail.com>
2025-01-10 23:11:20 +0200

    feat + ref (redeem-codes command): add zzz

--- commands/utils/redeem-codes.js
+++ commands/utils/redeem-codes.js
@@ -2,25 +2,43 @@
 require = global.alias(require)
 module.exports = {
 	init({ addCommand }){
-		const client = require( '@/instances/client' )
 		const discord = require( 'discord.js' )
+		const client = require( '@/instances/client' )
 		const Embed = require( '@/functions/Embed' )
 
-		const primogemEmojiID = `1063048589770756136`
-		const stellarJadeEmojiID = `1112055114916696114`
-
-		const GIRedeemLink = `https://genshin.hoyoverse.com/m/en/gift?code=`
-		const HSRRedeemLink = `https://hsr.hoyoverse.com/gift?code=`
+		const Games = {
+			gi: {
+				description: 'codes for Genshin Impact',
+				redeemLink: `https://genshin.hoyoverse.com/m/en/gift?code=`,
+				emojiID: `1063048589770756136`,
+			},
+			hsr: {
+				description: 'codes for Honkai: Star Rail',
+				redeemLink: `https://hsr.hoyoverse.com/gift?code=`,
+				emojiID: `1112055114916696114`,
+			},
+			zzz: {
+				description: 'codes for Zenless Zone Zero',
+				redeemLink: `https://zenless.hoyoverse.com/redemption?code=`,
+				emojiID: `1327366628622729310`,
+			},
+		}
 
 		const description = `sends fancy message with code redeem link(s)`
 		const pleaseSpecifyFlag = `Please specify for which game those code(s) are using one of flags`
 
+		const flags = Object
+			.entries( Games )
+			.map( ([key, game]) => [key, game.description] )
+
+		const flagArgument = Object
+			.keys( Games )
+			.map( flag => `--${flag}` )
+			.join( '|' )
+
 		addCommand({
 			aliases: 'redeem-codes codes',
-			flags: [
-				['gi', 'codes for genshin impact'],
-				['hsr', 'codes for honkai star rail'],
-			],
+			flags,
 			description: {
 				short: description,
 				full: [
@@ -28,50 +46,45 @@ module.exports = {
 					pleaseSpecifyFlag,
 				],
 				usages: [
-					[`<...codes>`, `<--gi|--hsr>`, ``],
+					[`<...codes>`, `<${flagArgument}>`, ``],
 				],
 			},
 			callback({ args, session }){
-				let codes = args.filter( s => /^\w{8,16}$/i.test(s) )
+				const codes = args.filter( s => /^\w{8,16}$/i.test(s) )
 
 				if( codes.length === 0 )
 					return session.update( `Give me some codes` )
 
-				const gi = args.flags.gi.specified
-				const hsr = args.flags.hsr.specified
-				let redeemLink, emojiID
+				const flags = Object
+					.keys( Games )
+					.filter( flag => args.flags[flag].specified )
 
-				if( gi ){
-					if( hsr )
-						return session.update( `You can't specify both flags` )
+				if( flags.length > 1 )
+					return session.update( `You can't specify multiple games` )
 
-					redeemLink = GIRedeemLink
-					emojiID = primogemEmojiID
-				} else if( hsr ){
-					redeemLink = HSRRedeemLink
-					emojiID = stellarJadeEmojiID
-				} else {
+				if( flags.length === 0 )
 					return session.update( pleaseSpecifyFlag )
-				}
 
-				const emoji = client.emojis.resolve( emojiID )
+				const game = Games[flags[0]]
+				const emojiString = client.emojis.resolve( game.emojiID )?.toString() || ''
+
 				const components = codes
 					.map( ( code, i ) => new discord.ButtonBuilder()
 						.setStyle( discord.ButtonStyle.Link )
-						.setURL( redeemLink + code )
-						.setLabel( `Redeem code #${i+1}` )
-						.setEmoji( emojiID )
+						.setURL( game.redeemLink + code )
+						.setLabel( `Redeem code #${i + 1}` )
+						.setEmoji( game.emojiID )
 					)
 					.map( button => new discord.ActionRowBuilder()
 						.addComponents( button )
 					)
 
-				codes = codes
-					.map( c => `${emoji.toString()} [${c}](${redeemLink + c})` )
+				const links = codes
+					.map( c => `${emojiString} [${c}](${game.redeemLink + c})` )
 					.join( '\n' )
 
 				const embed = Embed()
-					.setDescription( `Redeem codes:\n${codes}` )
+					.setDescription( `Redeem codes:\n${links}` )
 
 				return session.update({
 					embeds: [embed],

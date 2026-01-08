970f40b59efef3232ca5bd061cf882b5eb835693
Aoki <ger.vladislav2001@gmail.com>
2026-01-08 22:07:37 +0200

    disable: `lmgtfy` command

---
+++ disabled/commands/Utils/lmgtfy.js
@@ -0,0 +1,50 @@
+// eslint-disable-next-line no-global-assign
+require = global.alias(require)
+module.exports = {
+	init({ addCommand }){
+		const Embed = require( '@/functions/Embed' )
+		const wait = require( '@/functions/wait' )
+		const urlBase = 'https://lmgtfy.com/?q='
+
+		addCommand({
+			aliases: 'lmgtfy lmg whatis whats',
+			flags: [
+				['iie', `includes internet explorer`],
+			],
+			description: {
+				short: 'let me google that for you (or someone else)',
+				full: 'Googles stuff for you (or someone else)',
+				usages: [
+					[`<search request>`, 'googles $1'],
+				],
+			},
+			async callback({ session, args }){
+				const q = args.getRaw()
+				const iie = args.flags.iie.specified ? '&iie=1' : ''
+				const whatIs = args[-1].toLowerCase().startsWith( 'what' ) ? 'What is ' : ''
+				const timeout = 1337 + Math.random() * 3e3
+
+				if( !q )
+					return session.update( 'Usage: `-help lmgtfy`' )
+
+				const url = urlBase + encodeURI( ( whatIs + q ).replace( /\s+/g, '+' ) ) + iie
+
+				session.update( Embed()
+					.addFields({
+						name: `OK 👌. Googling \`${whatIs + q}\`...`,
+						value: 'Please wait a bit :^)'
+					})
+				)
+
+				await wait( timeout )
+
+				session.update( Embed()
+					.addFields({
+						name: 'Found!',
+						value: `Click here to find out ${whatIs.toLowerCase()}[${q}](${url})`,
+					})
+				)
+			},
+		})
+	}
+}
